4ae9858cb181a13a7dfc9e94117571acbacac5f6
Stephen Joseph <80311745+stephjoseph@users.noreply.github.com>
2022-02-28 12:21:56 +0800

    Update script.js

--- src/script.js
+++ src/script.js
@@ -7,14 +7,21 @@ window.onload = () => {
 
 checkbox.addEventListener("click", () => {
   prices.forEach((price) => {
-    let newPrice = price.innerHTML;
+    let newPrice = price.children[1].innerHTML;
     if (checkbox.checked) {
-      newPrice = (newPrice * 10 + 0.09).toFixed(2);
-      console.log(newPrice);
+      price.classList.add("opacity-0");
+      setTimeout(() => {
+        newPrice = (newPrice * 10 + 0.09).toFixed(2);
+        price.children[1].innerHTML = newPrice;
+        price.classList.remove("opacity-0");
+      }, 500);
     } else {
-      newPrice = (newPrice / 10 - 0.01).toFixed(2);
-      console.log(newPrice);
+      price.classList.add("opacity-0");
+      setTimeout(() => {
+        newPrice = (newPrice / 10 - 0.01).toFixed(2);
+        price.children[1].innerHTML = newPrice;
+        price.classList.remove("opacity-0");
+      }, 500);
     }
-    price.innerHTML = newPrice;
   });
 });
